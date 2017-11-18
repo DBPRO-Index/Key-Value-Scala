404d48de4bb167f4151c846519566eaf2459a3a5
phgrote <phigro161@gmail.com>
2017-11-18 08:33:16 +0100

    index

---
+++ src/main/scala/KeyValuePair.scala
@@ -0,0 +1,9 @@
+class KeyValuePair(newPair: (String, String)) {
+  val key: String = newPair._1
+  val value: String = newPair._2
+  val data:Array[Byte] = s"$key:$value\n".getBytes( "UTF-8")
+  val size: Int = data.length
+
+  override def toString: String = s"$key:$value"
+}
+
